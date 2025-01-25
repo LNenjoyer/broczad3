9e6ee50df16751b2441fdc95040036a1a998fb2c
LNenjoyer <ut_3779@zsp3zamosc.pl>
2025-01-25 11:32:17 +0100

    aaaaa

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,7 +1,18 @@
-import styles from './Button.module.css'
+//import styles from './Button.module.css'
 function Button(){
+    const styles={
+        backgroundColor: "aqua",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+      }
+
     return(
-        <button className={styles.button}>Click me</button>
+        //<button className={styles.button}>Click me</button>
+        <button style={styles}>Click me</button>
+
     );
 }
 export default Button
